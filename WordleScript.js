36747708b2ffec944712a87a5646659af73d7cfa
jackbayless <jackhunterbayless@gmail.com>
2026-03-21 17:51:21 -0700

    FULLY FUNCTIONAL

--- WordleScript.js
+++ WordleScript.js
@@ -1,13 +1,15 @@
-
 console.log("Page Loaded")
 
 let row = 0;
 let col = 0;
 let global_length = 5;
 let global_difficulty = 6;
+let row_full = false;
 let word = "BELLA"
 
 let EC2_URL = "https://www.jackbayless.club";
+set_word()
+console.log("Word set to: ", word)
 
 function setTile(row, col, letter) {
     console.log("Tile " + row + " " + col + " set to " + letter);
@@ -28,12 +30,19 @@ function setTile(row, col, letter) {
 }
 
 function increment_col () {
+    if (col === global_length - 1) {
+        row_full = true;
+    }
     if (col < global_length - 1) {
         col++;
     }
 }
 
 function decrement_col () {
+
+    if (col === global_length - 1) {
+        row_full = false;
+    }
     if (col > 0) {
         col--;
     }
@@ -72,7 +81,6 @@ function draw_tiles() {
 
         const container = document.createElement("div");
         container.classList.add("game_container");
-        document.body.appendChild(container);
 
         for (let j = 0; j < global_length; j++) {
 
@@ -91,21 +99,12 @@ function draw_tiles() {
 
 // placeholder before i set up server and get words through there -- ..
 async function set_word() {
-    let len = parseInt(global_length);
-
-    if (len === 3) word = "CAT";
-    else if (len === 4) word = "FART";
-    else if (len === 5) word = "BELLA";
-    else if (len === 6) word = "CLEARY";
-    else if (len === 7) word = "ICEKING";
-    else if (len === 8) word = "THINKPAD";
 
     let url = EC2_URL + "/wordle/getword?length=" + global_length;
 
     let r = await fetch(url);
     let data = await r.json();
-    console.log(data.word)
-    word = data.word;
+    word = data.word.toUpperCase();
 
 }
 
@@ -122,6 +121,7 @@ addEventListener('keydown', function(e) {
     }
 
     else if (e.key === "Backspace") {
+        e.preventDefault()
         setTile(row, col, "");
         decrement_col();
         console.log("Backspace");
@@ -129,52 +129,44 @@ addEventListener('keydown', function(e) {
 
     else if (e.key === "Enter") {
         e.preventDefault();
-        let score = 0;
-        console.log("Enter Clicked");
-        col = 0;
+        if (row_full) {
 
-        for (col; col < global_length; col++) {
-            let id = "" + row + "" + col;
-            let letter = document.getElementById(id).textContent;
+            let score = 0;
+            console.log("Enter Clicked");
+            col = 0;
 
-            let guess_value = check_letter(letter, col)
-            score += guess_value;
+            for (col; col < global_length; col++) {
+                let id = "" + row + "" + col;
+                let letter = document.getElementById(id).textContent;
 
-            if (guess_value === 2) update_tile_and_key(id, "green");
-            else if (guess_value === 1) update_tile_and_key(id, "yellow");
-            else update_tile_and_key(id, "grey");
+                let guess_value = check_letter(letter, col)
+                score += guess_value;
 
-            if (score === 2 * global_length) {
-                document.getElementById("result").textContent = "YOU WIN!!!!"
-                return;
+                if (guess_value === 2) update_tile_and_key(id, "green");
+                else if (guess_value === 1) update_tile_and_key(id, "yellow");
+                else update_tile_and_key(id, "grey");
+
+                if (score === 2 * global_length) {
+                    document.getElementById("result").textContent = "YOU WIN!!!!"
+                    return;
+                }
+
             }
-
-
+            row++;
+            col = 0;
+            row_full = false;
         }
-        row++;
-        col = 0;
-
-        if (row >= global_difficulty - 1) {
-            document.getElementById("result").textContent = "YOU LOSE :("
+        if (row >= global_difficulty) {
+            document.getElementById("result").textContent = "YOU LOSE :( WORD = " + word;
         }
         return;
 
 
-
-
-
     }
-
-
 
     console.log("Row: " + row + " col: " + col);
 
-// woo
-
-
-
-
-})
+});
 
 document.getElementById("apply_options_button").addEventListener('click', function(e) {
     console.log("apply clicked")
@@ -191,7 +183,14 @@ document.getElementById("apply_options_button").addEventListener('click', functi
     document.getElementById("tiles_container").innerHTML = "";
     document.getElementById("tiles_container").appendChild(draw_tiles());
 
+    let keys = document.getElementsByClassName("key_input");
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].style.backgroundColor = "";
+    }
+
+    document.getElementById("result").textContent = "";
+
+
     row = 0;
     col = 0;
-
-})
+});
